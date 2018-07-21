c4f76545dd294bd5c553a51f9eb19fda8cf688df
someone <something@apa.com>
2018-07-21 08:54:11 -0400

    Added routing

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'homeapge',
-  template: '<h1>Home</h1>'
+  template: '<h1>Homepage</h1>'
 })
 export class HomepageComponent {}
