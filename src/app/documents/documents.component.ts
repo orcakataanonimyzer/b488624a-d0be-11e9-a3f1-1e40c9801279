37ba5603dde8cf77218cddafdf8f5d4fcfe9dfcf
someone <something@apa.com>
2018-07-22 18:44:35 -0400

    Added Mock Data for Document

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
-export class DocumentsComponent {}
+export class DocumentsComponent {
+  documents: Document[] = [
+    {
+      "title": "My First Doc",
+      "description": "asdfasdfasdf asdfd",
+      "file_url": "http://google.com",
+      "updated_at": "11/11/16",
+      "image_url": "http://google.com",
+    },
+    {
+      "title": "My Second Doc",
+      "description": "asdfasdfasdf asdfd",
+      "file_url": "http://google.com",
+      "updated_at": "11/11/16",
+      "image_url": "http://google.com",
+    },
+    {
+      "title": "My Last Doc",
+      "description": "asdfasdfasdf asdfd",
+      "file_url": "http://google.com",
+      "updated_at": "11/11/16",
+      "image_url": "http://google.com",
+    }
+  ]
+}
