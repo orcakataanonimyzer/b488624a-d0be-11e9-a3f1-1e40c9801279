238071d25f4376cb22e713e834661387a8f5ae75
someone <something@apa.com>
2018-07-23 07:40:13 -0400

    populated proposal list with data

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent { }
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'ABC Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'davidjash2@gmail.com')
+  proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'davidjash2@gmail.com')
+  proposalThree: Proposal = new Proposal(300, 'Somethin Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'davidjash2@gmail.com')
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+}
