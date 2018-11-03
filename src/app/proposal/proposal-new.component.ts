2dcc56218377da2129cc117ca3c17429b9382aa6
someone <something@apa.com>
2018-11-03 10:32:12 -0400

    Added styles for proposal new component

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -3,7 +3,8 @@ import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposal-new',
-  templateUrl: 'proposal-new.component.html'
+  templateUrl: 'proposal-new.component.html',
+  styleUrls: ['proposal-new.component.css']
 })
 export class ProposalNewComponent {
   proposal = new Proposal;
