b87df8729f676111b796323839bc4e508bd5330a
someone <something@apa.com>
2018-11-03 12:43:19 -0400

    Added styles for home and proposal show pages

--- src/app/proposal/proposal-show.component.ts
+++ src/app/proposal/proposal-show.component.ts
@@ -4,7 +4,8 @@ import { Proposal } from "./proposal";
 
 @Component({
   selector: 'proposal-show',
-  templateUrl: 'proposal-show.component.html'
+  templateUrl: 'proposal-show.component.html',
+  styleUrls: ['proposal-show.component.css']
 })
 export class ProposalShowComponent implements OnInit {
   id: number;
