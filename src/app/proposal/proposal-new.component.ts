2f7931aafda6e4073d61c7d918bcb8b3749440c4
someone <something@apa.com>
2018-11-04 09:15:09 -0500

    Added capability to submit proposal

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs'
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   selector: 'proposal-new',
@@ -9,4 +11,20 @@ import { Proposal } from './proposal';
 export class ProposalNewComponent {
   proposal = new Proposal;
   submitted: boolean = false;
+
+  constructor(
+    private proposalService: ProposalService
+  ){}
+
+  createProposal(proposal){
+    this.submitted = true;
+    this.proposalService.createProposal(proposal)
+        .subscribe(
+          data => { return true },
+          error => {
+            console.log("Error saving proposal");
+            return Observable.throw(error);
+          }
+        )
+  }
 }
