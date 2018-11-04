b90f283e60f3241eff65a44a18b2b50c8e42c0b3
someone <something@apa.com>
2018-11-04 08:33:08 -0500

    Added a custom link function for proposal

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 import { Proposal } from './proposal';
 import { ProposalService } from "./proposal.service";
 
@@ -14,7 +15,8 @@ export class ProposalListComponent implements OnInit{
   mode = "Observable";
 
   constructor(
-    private proposalService: ProposalService
+    private proposalService: ProposalService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -27,5 +29,10 @@ export class ProposalListComponent implements OnInit{
           documents => this.proposals = documents,
           error => this.errorMessage = <any>error
         )
+  }
+
+  goToShow(proposal: Proposal): void {
+    let link = ["/proposal", proposal.id]
+    this.router.navigate(link)
   }
 }
