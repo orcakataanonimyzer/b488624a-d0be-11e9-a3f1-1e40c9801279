38327504f727f76a80aa89432eef8427984f7aeb
someone <something@apa.com>
2018-11-04 07:59:57 -0500

    Added capability to pull the proposal API

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Proposal } from './proposal';
+import { ProposalService } from "./proposal.service";
 
 @Component({
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list.component.css']
+  styleUrls: ['proposal-list.component.css'],
+  providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, 'ABC Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'davidjash2@gmail.com')
-  proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'davidjash2@gmail.com')
-  proposalThree: Proposal = new Proposal(300, 'Somethin Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'davidjash2@gmail.com')
-  proposals: Proposal[] = [
-    this.proposalOne,
-    this.proposalTwo,
-    this.proposalThree
-  ]
+export class ProposalListComponent implements OnInit{
+  proposals: Proposal[] = []
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private proposalService: ProposalService
+  ) {}
+
+  ngOnInit() {
+    this.getProposals()
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          documents => this.proposals = documents,
+          error => this.errorMessage = <any>error
+        )
+  }
 }
---
+++ src/app/proposal/proposal.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { Observable } from "rxjs";
+import { map, catchError } from 'rxjs/operators';
+
+import { Proposal } from './proposal';
+
+@Injectable()
+export class ProposalService{
+ private proposalsUrl='http://localhost:4202/proposals.json';
+ constructor(
+   private http: Http
+ ) {}
+
+ getProposals(): Observable<Proposal[]>{
+   return this.http.get(this.proposalsUrl)
+                   .pipe(map((response: Response) =>
+                           <Proposal[]>response.json()))
+                  .pipe(catchError(this.handleError))
+ }
+ private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
