594925a956467b0e95aba4d274ca10329ed4e748
someone <something@apa.com>
2019-02-03 13:53:02 -0500

    removed documents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
-import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component'
 import { ProposalNewComponent} from './proposal/proposal-new.component'
 import { ProposalShowComponent} from './proposal/proposal-show.component'
